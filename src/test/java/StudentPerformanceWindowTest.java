afc931e286d05694e65d1e8d003ebdbb53da79d9
farjanarahman <rahmanfarjana313@gmail.com>
2025-03-15 17:03:49 +0600

    5th Commit

--- src/test/java/StudentPerformanceWindowTest.java
+++ src/test/java/StudentPerformanceWindowTest.java
@@ -11,4 +11,12 @@ class StudentPerformanceWindowTest {
         assertSame("Excellent! Keep up the great work!", performanceWindow.generatePerformanceTip(3.9));
         assertNotSame("Needs Improvement! Focus on weaker subjects.", performanceWindow.generatePerformanceTip(3.5));
     }
+    @Test
+    void testCalculateGPA() {
+        StudentPerformanceWindow performanceWindow = new StudentPerformanceWindow(new ArrayList<>());
+
+        assertFalse(performanceWindow.calculateGPA(55) == 3.0);
+        assertFalse(performanceWindow.calculateGPA(75) == 2.0);
+        //check gpa calc
+    }
 }
